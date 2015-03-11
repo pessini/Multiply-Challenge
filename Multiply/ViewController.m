c7156dd03c815d98e7383fe4ead721fc62e29471
Leandro Pessini <pessini@Leandros-MacBook-Air.local>
2015-03-10 22:03:59 -0300

    Initial commit

--- Multiply/ViewController.m
+++ Multiply/ViewController.m
@@ -10,18 +10,107 @@
 
 @interface ViewController ()
 
+
+@property (weak, nonatomic) IBOutlet UILabel *numberLabel;
+@property (weak, nonatomic) IBOutlet UILabel *multiplierLabel;
+@property (weak, nonatomic) IBOutlet UILabel *answerLabel;
+@property (weak, nonatomic) IBOutlet UISlider *multiplierSlider;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *operatorSegmentControl;
+
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    UITextView *textView = [[UITextView alloc] initWithFrame:CGRectZero];
+    [textView setKeyboardType:UIKeyboardTypeNumberPad];
+    [textView.delegate self];
+    [self.view addSubview:textView];
+    [textView becomeFirstResponder];
+
+
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark textView Delegates
+
+-(BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
+{
+
+    self.numberLabel.text = textView.text;
+    return YES;
 }
+
+#pragma mark IBAction
+
+- (IBAction)sliderValueChanged:(UISlider *)sender
+{
+
+    // When the user drags the slider and changes its value, adjust the value of multiplierLabel to be the same
+    self.multiplierLabel.text = [NSString stringWithFormat:@"%i", (int)self.multiplierSlider.value];
+
+}
+
+
+- (IBAction)onCalculateButtonPressed:(UIButton *)sender
+{
+    // converting the number entered to an intValue
+    int number = [self.numberLabel.text intValue];
+
+    // converting the multiplier to an intValue
+    int multiplier = [self.multiplierLabel.text intValue];
+
+    
+    int result;
+    if (self.operatorSegmentControl.selectedSegmentIndex == 0)
+    {
+        result = number * multiplier;
+    }
+    else
+    {
+        result = number / multiplier;
+    }
+
+
+    // checking if the result is a multiple of 3 and 5
+    BOOL isMultipleOfThree = !(result % 3);
+    BOOL isMultipleOfFive = !(result % 5);
+
+    if (isMultipleOfThree && isMultipleOfFive)
+    {
+        self.answerLabel.text = @"fizzbuzz";
+    }
+    else if (isMultipleOfThree)
+    {
+        self.answerLabel.text = @"fizz";
+    }
+    else if (isMultipleOfFive)
+    {
+        self.answerLabel.text = @"buzz";
+    }
+    else
+    {
+        self.answerLabel.text = [NSString stringWithFormat:@"%i", result];
+    }
+
+    // change the viewController's background depending on the result
+    if (result >= 20)
+    {
+        self.view.backgroundColor = [UIColor greenColor];
+    }
+    else
+    {
+        self.view.backgroundColor = [UIColor whiteColor];
+
+    }
+
+    NSLog(@"%i", result);
+
+
+}
+
 
 @end
